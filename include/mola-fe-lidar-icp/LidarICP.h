f6fcd27bcc1c1f4e38efb56c136ea49cabff301e
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2018-12-17 12:21:12 +0100

    first basic working version of ICP for odometry

--- include/mola-fe-lidar-icp/LidarICP.h
+++ include/mola-fe-lidar-icp/LidarICP.h
@@ -12,22 +12,58 @@
 #pragma once
 
 #include <mola-kernel/FrontEndBase.h>
+#include <mola-kernel/WorkerThreadsPool.h>
+#include <mrpt/maps/CPointsMap.h>
+#include <mrpt/slam/CICP.h>
 
 namespace mola
 {
-/**
+/** A front-end for Lidar/point-cloud odometry & SLAM.
+ *
  * \ingroup mola_fe_lidar_icp_grp */
 class LidarICP : public FrontEndBase
 {
    public:
     LidarICP();
+    virtual ~LidarICP() override = default;
 
     // See docs in base class
     void initialize(const std::string& cfg_block) override;
     void spinOnce() override;
     void onNewObservation(CObservation::Ptr& o) override;
 
+    /** Re-initializes the front-end */
+    void reset();
+
+    struct Parameters
+    {
+        /** Minimum time (seconds) between scans for  */
+        double min_time_between_scans_{0.2};
+
+        mrpt::slam::CICP::TConfigParams mrpt_icp{};
+    };
+
+    /** Algorithm parameters */
+    Parameters params_;
+
    private:
+    /** The worker thread pool with 1 thread for processing incomming scans */
+    mola::WorkerThreadsPool worker_pool_{1};
+
+    /** All variables that hold the algorithm state */
+    struct MethodState
+    {
+        mrpt::Clock::time_point     last_obs_tim{};
+        mrpt::maps::CPointsMap::Ptr last_points{};
+        CObservation::Ptr           last_obs{};
+        mrpt::slam::CICP            mrpt_icp;
+    };
+
+    MethodState state_;
+
+    /** Here happens the actual processing, invoked from the worker thread pool
+     * for each incomming observation */
+    void doProcessNewObservation(CObservation::Ptr& o);
 };
 
 }  // namespace mola
--- src/LidarICP.cpp
+++ src/LidarICP.cpp
@@ -17,6 +17,9 @@
  */
 
 #include <mola-fe-lidar-icp/LidarICP.h>
+#include <mrpt/maps/CSimplePointsMap.h>
+#include <mrpt/poses/CPose3DPDFGaussian.h>
+#include <mrpt/system/datetime.h>
 #include <yaml-cpp/yaml.h>
 
 using namespace mola;
@@ -28,12 +31,79 @@ LidarICP::LidarICP() = default;
 void LidarICP::initialize(const std::string& cfg_block)
 {
     //
+    MRPT_TODO("Load params into params_");
+
+    // Default params:
+    params_.mrpt_icp.corresponding_points_decimation = 10;
 }
 void LidarICP::spinOnce()
 {
     //
 }
+
+void LidarICP::reset() { state_ = MethodState(); }
+
 void LidarICP::onNewObservation(CObservation::Ptr& o)
 {
-    //
+    worker_pool_.enqueue(&LidarICP::doProcessNewObservation, this, o);
+}
+
+// here happens the main stuff:
+void LidarICP::doProcessNewObservation(CObservation::Ptr& o)
+{
+    MRPT_TRY_START
+    ASSERT_(o);
+
+    // Only process pointclouds that are sufficiently apart in time:
+    const auto this_obs_tim = o->timestamp;
+    if (state_.last_obs_tim != mrpt::Clock::time_point() &&
+        mrpt::system::timeDifference(state_.last_obs_tim, this_obs_tim) <
+            params_.min_time_between_scans_)
+    {
+        // Drop observation.
+        return;
+    }
+
+    auto       this_obs_points = mrpt::maps::CSimplePointsMap::Create();
+    const bool have_points     = this_obs_points->insertObservationPtr(o);
+
+    // Store for next step:
+    // auto last_obs_tim   = state_.last_obs_tim;
+    auto last_obs       = state_.last_obs;
+    auto last_points    = state_.last_points;
+    state_.last_obs     = o;
+    state_.last_obs_tim = this_obs_tim;
+    state_.last_points  = this_obs_points;
+
+    // First time we cannot do ICP since we need at least two pointclouds:
+    if (!last_points)
+    {
+        MRPT_LOG_DEBUG("First pointcloud: skipping ICP.");
+        return;
+    }
+
+    if (!have_points)
+    {
+        MRPT_LOG_WARN_STREAM(
+            "Observation of type `"
+            << o->GetRuntimeClass()->className
+            << "` could not be converted into a pointcloud. Doing nothing.");
+        return;
+    }
+
+    // Register point clouds using any of the available ICP algorithms:
+    mrpt::poses::CPose3DPDFGaussian initial_guess;
+    mrpt::slam::CICP::TReturnInfo   ret_info;
+    // Call ICP:
+    auto rel_pose_pdf = state_.mrpt_icp.Align3DPDF(
+        last_points.get(), this_obs_points.get(), initial_guess,
+        nullptr /*running_time*/, &ret_info);
+
+    const mrpt::poses::CPose3D rel_pose = rel_pose_pdf->getMeanVal();
+
+    MRPT_LOG_DEBUG_FMT(
+        "MRPT ICP done: goodness=%.03f rel_pose=%s", ret_info.goodness,
+        rel_pose.asString().c_str());
+
+    MRPT_TRY_END
 }
